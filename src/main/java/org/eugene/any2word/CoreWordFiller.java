894e6625aa1cee6ffa0f3dd43b5b76e3b079ce08
nergal-perm <nergal@psu.ru>
2015-01-05 11:21:06 +0600

    succeeded to add custom xml part to Word template

--- src/main/java/org/eugene/any2word/CoreWordFiller.java
+++ src/main/java/org/eugene/any2word/CoreWordFiller.java
@@ -3,30 +3,98 @@ package org.eugene.any2word;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 
-import org.docx4j.Docx4J;
+import org.docx4j.XmlUtils;
+import org.docx4j.customXmlProperties.DatastoreItem;
+import org.docx4j.model.datastorage.CustomXmlDataStorage;
+import org.docx4j.model.datastorage.CustomXmlDataStorageImpl;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.docx4j.openpackaging.exceptions.InvalidFormatException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.docx4j.openpackaging.parts.CustomXmlDataStoragePart;
-import org.docx4j.samples.CreateDocx;
+import org.docx4j.openpackaging.parts.CustomXmlDataStoragePropertiesPart;
+import org.docx4j.openpackaging.parts.Parts;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
 
 public class CoreWordFiller {
 	private WordprocessingMLPackage wordMLPackage;
 
-	public CoreWordFiller(WordprocessingMLPackage wordDoc, File xmlFile) throws FileNotFoundException, Docx4JException {
+	/**
+	 * Creates in-memory representation of a Word template, adds provided
+	 * XML file to it, manages data binding to ContentControls inside the
+	 * template
+	 * 
+	 * @param wordDoc - Word template file
+	 * @param xmlFile - XML file with custom data to use with Word template
+	 * @throws FileNotFoundException
+	 * @throws Docx4JException
+	 */
+	public CoreWordFiller(WordprocessingMLPackage wordDoc, File xmlFile)
+			throws FileNotFoundException, Docx4JException {
 		setWordDocument(wordDoc);
-		Docx4J.bind(wordMLPackage, new FileInputStream(xmlFile), Docx4J.FLAG_BIND_INSERT_XML);
-		// add XML file as part of the package
-		String itemID = CustomXmlUtils.getCustomXmlItemId(wordMLPackage);
-		CustomXmlDataStoragePart xmlPart = (CustomXmlDataStoragePart) wordMLPackage.getCustomXmlDataStorageParts().get(itemID);
-		if (xmlPart==null) {
-			throw new Docx4JException("Couldn't find Custom XML Part in a document");
+		
+		// Create custom XML part infrastructure
+		CustomXmlDataStoragePart xmlPart = injectCustomXmlDataStoragePart(
+				wordDoc.getMainDocumentPart(), wordDoc.getParts(), xmlFile);
+		if (xmlPart == null) {
+			throw new Docx4JException(
+					"Couldn't find Custom XML Part in a document");
 		}
-		xmlPart.getData().setDocument(new FileInputStream(xmlFile));
 		
+		// Populate custom XML part with data provided
+		// xmlPart.getData().setDocument(new FileInputStream(xmlFile));
+		addProperties(xmlPart);
+		CustomXmlDataStorage customXmlDataStorage = xmlPart.getData();
+		// DEBUG only
+		System.out.println(XmlUtils.w3CDomNodeToString(customXmlDataStorage.getDocument()));		
 		
-		
+		// Bind ContentControls to data in XML file
 	}
+
+	/**
+	 * 
+	 * @param base
+	 * @param parts
+	 * @return
+	 */
+	private CustomXmlDataStoragePart injectCustomXmlDataStoragePart(
+			MainDocumentPart base, Parts parts, File xmlFile) {
+
+		CustomXmlDataStoragePart customXmlDataStoragePart = null;
+		try {
+
+			customXmlDataStoragePart = new CustomXmlDataStoragePart(parts);
+			CustomXmlDataStorage data = new CustomXmlDataStorageImpl();
+			data.setDocument(createCustomXmlDocument(xmlFile));
+			customXmlDataStoragePart.setData(data);
+			base.addTargetPart(customXmlDataStoragePart);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return customXmlDataStoragePart;
+	}
+
+	private InputStream createCustomXmlDocument(File xmlFile) {
+		try {
+			return new FileInputStream(xmlFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+    public static void addProperties(CustomXmlDataStoragePart customXmlDataStoragePart) throws InvalidFormatException {
+        CustomXmlDataStoragePropertiesPart part = new CustomXmlDataStoragePropertiesPart();
+        org.docx4j.customXmlProperties.ObjectFactory of = new org.docx4j.customXmlProperties.ObjectFactory();
+        DatastoreItem dsi = of.createDatastoreItem();
+        String newItemId = "{DD6E220C-54BC-47B3-8AE8-A0A61D4934FF}".toLowerCase();                              
+        dsi.setItemID(newItemId);
+        part.setJaxbElement(dsi );
+        customXmlDataStoragePart.addTargetPart(part);
+    }
+
+
 
 	public WordprocessingMLPackage getWordDocument() {
 		return wordMLPackage;
@@ -35,7 +103,5 @@ public class CoreWordFiller {
 	public void setWordDocument(WordprocessingMLPackage wordMLPackage) {
 		this.wordMLPackage = wordMLPackage;
 	}
-	
-	
 
 }
--- src/test/java/org/eugene/any2word/CoreWordFillerTest.java
+++ src/test/java/org/eugene/any2word/CoreWordFillerTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+import org.docx4j.XmlUtils;
+import org.docx4j.model.datastorage.CustomXmlDataStorage;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.junit.Before;
@@ -13,7 +15,7 @@ import org.junit.Test;
 public class CoreWordFillerTest {
 	private WordprocessingMLPackage wordDoc;
 	private File xmlFile;
-	
+
 	@Before
 	public void initialize() {
 		String resDocName = "SampleTemplate.docx";
@@ -23,14 +25,15 @@ public class CoreWordFillerTest {
 		xmlFile = new File(classLoader.getResource(resXmlName).getFile());
 		try {
 			wordDoc = WordprocessingMLPackage.load(docxFile);
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace(System.out);
 		}
 	}
-	
+
 	@Test
-	public void testCoreWordFillerCreation() {
+	public void testXMLInjectionUponCreation() {
 		CoreWordFiller target = null;
 		try {
 			target = new CoreWordFiller(wordDoc, xmlFile);
@@ -39,5 +42,19 @@ public class CoreWordFillerTest {
 			e.printStackTrace();
 		}
 		assertNotNull(target.getWordDocument().getCustomXmlDataStorageParts());
+	}
+
+	@Test
+	public void testXMLDataCanBeRetrieved() {
+		CoreWordFiller target = null;
+		String itemId = "";
+		try {
+			target = new CoreWordFiller(wordDoc, xmlFile);
+			itemId = CustomXmlUtils.getCustomXmlItemId(target.getWordDocument()).toLowerCase();
+		} catch (Docx4JException | FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		System.out.print(itemId);
+		assertNotEquals("Item ID is empty", "", itemId);
 	}
 }
